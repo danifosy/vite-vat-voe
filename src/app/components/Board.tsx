4d3360fdb90d2d247735c8aa4e5a76db613bcdd6
danifosy <mail@daniha.de>
2021-08-16 17:43:17 +0200

    Work through tutorial

--- src/app/components/Board.tsx
+++ src/app/components/Board.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 
 function Board(): JSX.Element {
-  function renderSquare(i: number) {
-    return <Square />;
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i: number) {
+    const newSquares = squares.slice();
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
   }
 
-  const status = 'Next player: X';
+  function renderSquare(i: number) {
+    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+  }
+
+  const status = 'Next player: ' + (xIsNext ? 'X' : 'O');
 
   return (
     <div>
