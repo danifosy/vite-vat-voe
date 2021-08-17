0e164497039d721f55967c80e27d660ee7e8862b
danifosy <mail@daniha.de>
2021-08-17 21:45:58 +0200

    Add type Square to Board.tsx

--- src/app/components/Square.tsx
+++ src/app/components/Square.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-type Props = {
+type SquareProps = {
   value: string;
   onClick: () => void;
 };
 
-function Square(props: Props): JSX.Element {
+function Square(props: SquareProps): JSX.Element {
   return (
     <button className="square" onClick={() => props.onClick()}>
       {props.value}
